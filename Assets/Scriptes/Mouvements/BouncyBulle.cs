2b2555ef33b8ff9f63224bd29c0efbf98ae5b43a
Onilaf <onilafoxy@gmail.com>
2025-01-25 17:29:51 +0100

    sdggdsihdsg

--- Assets/Scriptes/Mouvements/BouncyBulle.cs
+++ Assets/Scriptes/Mouvements/BouncyBulle.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class BouncyBulle : MonoBehaviour {
     [SerializeField] private float bounciness;
-    
+
     private void OnTriggerEnter(Collider other) {
+        float tempBounciness = bounciness;
+        if (other.gameObject.transform.position.x < this.gameObject.transform.position.x) {
+            tempBounciness *=
+                1 + (this.gameObject.transform.position.x - other.gameObject.transform.position.x) * 0.15f;
+        }
+
         other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(
             other.gameObject.GetComponent<Rigidbody>().velocity.x,
-            (-other.gameObject.GetComponent<Rigidbody>().velocity.y) * bounciness);
+            (-other.gameObject.GetComponent<Rigidbody>().velocity.y) * tempBounciness);
     }
 }
