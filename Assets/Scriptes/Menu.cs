1e69f70bca83fd49eecd49c88488b263bc3f63fe
Onilaf <onilafoxy@gmail.com>
2025-01-26 14:42:14 +0100

    Add Settings Menu

--- Assets/Scriptes/Menu.cs
+++ Assets/Scriptes/Menu.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
     public static Menu Instance { get; private set; }
     public bool isPaused;
     
     public CanvasGroup gameCanvas;
-    public GameObject settingsCanvas;
+    public GameObject menuPanel;
+    public GameObject settingsPanel;
+    
+    public AudioSource musicSource;
+    public Slider volumeSlider;
     
     void Awake() {
         if (Instance != null && Instance != this) {
@@ -18,23 +24,31 @@ public class Menu : MonoBehaviour {
     
     public void Start() {
         gameCanvas.interactable = true;
-        settingsCanvas.SetActive(false);
+        menuPanel.SetActive(false);
         isPaused = false;
-    }
-
-    public void GetBubbleAvailable() {
-        
     }
 
     public void OpenMenu(bool value) {
         gameCanvas.interactable = !value;
-        settingsCanvas.SetActive(value);
+        menuPanel.SetActive(value);
+        settingsPanel.SetActive(false);
         Time.timeScale = value ? 0 : 1;
         isPaused = value;
     }
     
     public void OpenSettings() {
-        
+        settingsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+    
+    public void CloseSettings() {
+        menuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+    
+    public void SetVolume(float volume) {
+        musicSource.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
     }
 
     public void QuitLevel() {
